7aeb2b2f2a77b0686a8912e3e6be9ee29f9d4c72
JSTawhida <tawhidajuthi32@gmail.com>
2025-04-16 11:13:05 +0100

    Update main.js

--- Services_Page/main.js
+++ Services_Page/main.js
@@ -1,11 +1,9 @@
 const nav = document.querySelector("nav");
 
-window.addEventListener("scroll",() =>){
-    if(window.pageXOffset > 60){
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 60) {
         nav.classList.add("scrolled");
-    }
-    else
-    {
+    } else {
         nav.classList.remove("scrolled");
     }
-}
+});
