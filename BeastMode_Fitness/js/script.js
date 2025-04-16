122492edbdd7955ef034750d3c461ab1452692a2
Rajan0601 <extraction.rkuk@gmail.com>
2025-04-16 09:00:50 +0545

    Rajan: Initial project setup and Home section

--- BeastMode_Fitness/js/script.js
+++ BeastMode_Fitness/js/script.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("BeastMode Fitness website is ready!");
-});
+$(document).ready(function () {
+    $(".hamburger").click(function () {
+      $("nav ul").toggleClass("show");
+    });
+  
+    $("nav a").click(function (e) {
+      if (this.hash !== "") {
+        e.preventDefault();
+        const target = this.hash;
+        $("html, body").animate({ scrollTop: $(target).offset().top - 60 }, 800);
+      }
+    });
+  });
+  
