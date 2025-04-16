ec46e3d115e2a39237e608fcb56a5eac0b37f1f2
suyog725 <pokhrelsuyog17@gmail.com>
2025-04-16 09:24:45 +0545

    Suyog: Added contact section with modal and social links

--- BeastMode_Fitness/js/script.js
+++ BeastMode_Fitness/js/script.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("BeastMode Fitness website is ready!");
 });
+const popup = $("#popupModal");
+const form = $("#contactForm");
+
+form.on("submit", function (e) {
+  e.preventDefault();
+  popup.show();
+  form[0].reset();
+});
+
+$(".closeBtn").click(function () {
+  popup.hide();
+});
+
+$(window).click(function (e) {
+  if ($(e.target).is("#popupModal")) {
+    popup.hide();
+  }
+});
